9824bdc8aaa18fd573ea2ea1e7df482ae45163c1
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 05:02:28 +0530

    add file

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,14 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/envConfig";
+import { TokenPayload } from "../types/authTypes";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        userId: string;
-        email: string;
-      };
+      user?: TokenPayload;
     }
   }
 }
@@ -18,14 +16,13 @@ const authMiddleware = (
   res: Response,
   next: NextFunction
 ): void => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    res.status(401).json({ message: "Access denied. No token provided." });
+    return;
+  }
   try {
-    const token = req.header("Authorization");
-
-    if (!token) {
-      res.status(401).json({ message: "Access denied. No token provided." });
-      return;
-    }
-
     const decoded = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET) as {
       userId: string;
       email: string;
