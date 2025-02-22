300bcd6b1dbfba495f43c63a4fafd69e3f67abd8
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 01:59:18 +0530

    modify code in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,23 @@
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
-
-dotenv.config();
+import authMiddleware from "./middleware/authMiddleware";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(
-    process.env.MONGO_URI as string,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+// mongoose
+//   .connect(
+//     process.env.MONGO_URI as string,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     } as mongoose.ConnectOptions
+//   )
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
 
@@ -29,6 +25,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res
+    .status(200)
+    .json({ message: "You have accessed a protected route!", user: req.user });
 });
+
+export default app;
