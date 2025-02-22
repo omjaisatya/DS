4cc2abad4399acfe8f2cfbc965ac13dd1b1bcdfc
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 02:22:28 +0530

    add aws s3 upload

---
+++ backend/src/utils/s3Upload.ts
@@ -0,0 +1,33 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+import multer from "multer";
+import multerS3 from "multer-s3";
+
+dotenv.config();
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+  region: process.env.AWS_REGION,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME as string,
+    acl: "public-read",
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(
+        null,
+        `profile-pictures/${Date.now().toString()}-${file.originalname}`
+      );
+    },
+  }),
+});
+
+export default upload;
