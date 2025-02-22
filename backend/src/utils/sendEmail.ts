ab75d5a43d4a7e990a4c4f53ec2ffec92b47eb1c
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 01:58:52 +0530

    fix error

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,20 +1,18 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
+import * as nodemailer from "nodemailer";
+import { EMAIL_PASS, EMAIL_USER } from "../config/envConfig";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
   },
 });
 
 const sendEmail = async (to: string, subject: string, text: string) => {
   try {
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: EMAIL_USER,
       to,
       subject,
       text,
