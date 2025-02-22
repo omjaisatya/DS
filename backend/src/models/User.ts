abf02806f81e17f3821e83bd8e688dab9cbbbd2a
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 05:01:35 +0530

    modify user models

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,23 +1,43 @@
-import mongoose from "mongoose";
+import mongoose, { ObjectId } from "mongoose";
 
-const UserSchema = new mongoose.Schema(
+interface Address extends mongoose.Types.Subdocument {
+  label: string;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
+
+interface User extends mongoose.Document {
+  email: string;
+  name: string;
+  phone: string;
+  profilePicture: string;
+  addresses: mongoose.Types.DocumentArray<Address>;
+}
+
+const AddressSchema = new mongoose.Schema<Address>(
+  {
+    label: { type: String, default: "Home" },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const UserSchema = new mongoose.Schema<User>(
   {
     email: { type: String, required: true, unique: true },
-    name: { type: String },
-    phone: { type: String },
-    addresses: [
-      {
-        street: String,
-        city: String,
-        state: String,
-        zip: String,
-        country: String,
-      },
-    ],
-    loyaltyPoints: { type: Number, default: 0 },
+    name: { type: String, default: "" },
+    phone: { type: String, default: "" },
     profilePicture: { type: String, default: "" },
+    addresses: { type: [AddressSchema], default: [] },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.model<User>("User", UserSchema);
