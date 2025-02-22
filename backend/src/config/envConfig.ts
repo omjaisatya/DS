a72aec9d3ba99c102c50ec6fb70782c5c2ca7c25
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 05:03:05 +0530

    add more env list

--- backend/src/config/envConfig.ts
+++ backend/src/config/envConfig.ts
@@ -1,10 +1,28 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const { MONGO_URI, PORT, EMAIL_USER, EMAIL_PASS, JWT_SECRET } = process.env;
+const {
+  MONGO_URI,
+  PORT,
+  EMAIL_USER,
+  EMAIL_PASS,
+  JWT_SECRET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+} = process.env;
 
 if (!MONGO_URI) {
   throw new Error("MONGODB_URL is not defined in the environment variables");
 }
 
-export { MONGO_URI, PORT, EMAIL_PASS, EMAIL_USER, JWT_SECRET };
+export {
+  MONGO_URI,
+  PORT,
+  EMAIL_PASS,
+  EMAIL_USER,
+  JWT_SECRET,
+  AWS_ACCESS_KEY_ID,
+  AWS_REGION,
+  AWS_SECRET_ACCESS_KEY,
+};
