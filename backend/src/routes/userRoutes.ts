2b0735d3e12c3c8e5140171e16b9d31210e91a9d
omjaisatya <satyaprakash451256@gmail.com>
2025-02-23 05:00:32 +0530

    add upload route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,13 +2,22 @@ import express from "express";
 import {
   updateUserProfile,
   getUserProfile,
+  uploadProfilePicture,
 } from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
+import upload from "../utils/s3Upload";
 
 const router = express.Router();
 
 router.get("/profile", authMiddleware, getUserProfile);
 
 router.put("/profile", authMiddleware, updateUserProfile);
+
+router.post(
+  "/profile/upload",
+  authMiddleware,
+  upload.single("profilePicture"),
+  uploadProfilePicture
+);
 
 export default router;
